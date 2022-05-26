a2f51975d5a1b967a04da8ea3079afcc59e89e69
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-05-26 01:02:39 -0300

    Adicionando métodos de busca enterprises

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,12 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
+	"github.com/gin-gonic/gin"
+)
+
+type Enterprise struct {
+	ID                 string      `json:"_id"`
+	LegalIdentifier    string      `json:"legalIdentifier"`
+	LogoUrl            string      `json:"LogoUrl"`
+	IsFilial           string      `json:"isFilial"`
+	BusinessName       string      `json:"businessName"`
+	FantasyName        string      `json:"fantasyName"`
+	RegistrationStatus string      `json:"registrationStatus"`
+	LegalNature        string      `json:"legalNature"`
+	CNAEPrincipal      string      `json:"CNAEPrincipal"`
+	CNAESecondary      string      `json:"CNAESecondary"`
+	Users              []string    `json:"users"`
+	Customers          []Customer  `json:"customers"`
+	Employeers         []Employeer `json:"employeers"`
+}
+
+type Customer struct {
+	ID              string `json:"_id"`
+	Email           string
+	EnterpriseId    string
+	FullName        string
+	LegalNature     string
+	LegalIdentifier string
+	Phones          []string
+	CreatedDate     time.Time
+	CreatedBy       string
+	UpdatedDate     time.Time
+	UpdatedBy       string
+}
+
+type Employeer struct {
+}
+
+var enterprises = []Enterprise{
+	{ID: "1", BusinessName: "Seegal Comercio do Brasil LTDA"},
+	{ID: "2", BusinessName: "Acetime Soluções Digitais LTDA"},
+}
+
+func getEnterprises(c *gin.Context) {
+	fmt.Println("Start to get enterprises")
+	c.IndentedJSON(http.StatusOK, enterprises)
+}
+
+func createEnterprise(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, enterprises)
+}
+
+func updateEnterprise(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, enterprises)
+}
+
+func deleteEnterprise(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, enterprises)
 }
 
 func main() {
-	fmt.Println(Hello("api"))
+	router := gin.Default()
+
+	// Enterprises
+	router.GET("/enterprises", getEnterprises)
+	router.POST("/enterprises", createEnterprise)
+	router.PUT("/enterprises/:id", updateEnterprise)
+	router.DELETE("/enterprises/:id", deleteEnterprise)
+
+	router.Run("localhost:5000")
 }
